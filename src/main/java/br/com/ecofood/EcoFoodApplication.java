70941c0e75c2da39b2ec2843b25b0c4141a5f9f1
João Candido <joaopcandidocosta@gmail.com>
2026-09-08 22:16:04 -0300

    Configura estrutura inicial do backend

---
+++ src/main/java/br/com/ecofood/EcoFoodApplication.java
@@ -0,0 +1,12 @@
+package br.com.ecofood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoFoodApplication.class, args);
+    }
+}
